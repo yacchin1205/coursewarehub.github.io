c3b99ab3285a1e465e24b84c253f68df19d925ca
yacchin1205 <yacchin1205@users.noreply.github.com>
2022-05-20 02:45:56 +0000

    jekyll build from Action c56b71cf429b04c2fe82e1ab6148e8f365a883a1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "CoursewareHub利用事例 - 群馬大学 井上先生",
+        "excerpt":"CoursewareHubの利用事例を国立情報学研究所 学術基盤オープンフォーラム2021にて 群馬大学・数理データ科学教育研究センター 井上先生にCoursewareHubの利用事例をご紹介いただきました！   資料は以下のリンクからご参照いただけます。     ","categories": [],
+        "tags": ["利用例","群馬大学"],
+        "url": "/2021/08/10/gunma-u.html",
+        "teaser": null
+      }]
